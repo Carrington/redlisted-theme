5ac7ca4aafd24708467dbf0ad078caf2859b1c16
dan <dan@dev.noblevitae.com>
2013-11-19 21:10:23 +0000

    css fix for solo pages

--- template-novels.php
+++ template-novels.php
@@ -3,7 +3,7 @@
 	<!-- section -->
 	<div role="content" class="content">
 	
-		<h2><?php the_title(); ?></h2>
+		<h2 class='page-title'><?php the_title(); ?></h2>
 	
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	
